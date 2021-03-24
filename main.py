efba82884f419e82fab7265cef3969ddd03becba
Lucas Peixoto Gonçalves <lucaspeixotopro@gmail.com>
2021-03-24 20:59:00 -0300

    buttons simplified

--- main.py
+++ main.py
@@ -1,79 +1,68 @@
-from tkinter import * 
+import tkinter as tk
+from tkinter import ttk
 
-class MainApp(Frame):
+BUTTONS = [
+   ['(', ')', 'CE', 'C'],
+   ['7', '8', '9', '/'],
+   ['4', '5', '6', '*'],
+   ['1', '2', '3', '-'],
+   ['0', '.', '=', '+'],
+]
+
+class MainApp(tk.Frame):
    def __init__(self, master=None):
       super().__init__(master)
       self.master = master
 
-      self.screen = Entry(
+      self.screen = tk.Entry(
          bg='#333', 
          fg='#eee',
          font=('Arial', 15),
-         justify=RIGHT,
+         justify=tk.RIGHT,
          bd=0,
       )
-      self.screen.bind('<1>', self.entry_active)
+      self.screen.bind('<1>', self.clean_when_error)
       self.screen.grid(columnspan=4, ipadx=33 ,ipady=10)
 
-      self.create_buttons()
+      self.create_buttons(buttons=BUTTONS)
 
 
-   def create_buttons(self):
-      buttons = [
-         [
-            Button(text='(', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('(')),
-            Button(text=')', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text(')')),
-            Button(text='CE', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('CE')),
-            Button(text='C', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('C')),
-         ],
-         [
-            Button(text='7', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('7')),
-            Button(text='8', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('8')),
-            Button(text='9', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('9')),
-            Button(text='/', bg='#222', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('/')),
-         ],
-         [
-            Button(text='4', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('4')),
-            Button(text='5', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('5')),
-            Button(text='6', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('6')),
-            Button(text='*', bg='#222', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('*')),
-         ],
-         [
-            Button(text='1', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('1')),
-            Button(text='2', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('2')),
-            Button(text='3', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('3')),
-            Button(text='-', bg='#222', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('-')),
-         ],
-         [
-            Button(text='0', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('0')),
-            Button(text='.', bg='#111', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('.')),
-            Button(text='=', bg='#ff3030', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('=')),
-            Button(text='+', bg='#222', fg='#eee', font=('Arial', 15), width=6, height=2, bd=0, command=lambda: self.change_text('+'))
-         ]
-      ]
+   def create_buttons(self, buttons):
+      '''
+      Function that creates the Button objects based on a matrix
+      '''
+      for i in range(len(buttons)):
+         for j in range(len(buttons[i])):
+            btn = tk.Button(
+               text=buttons[i][j],
+               bg='#111',
+               fg='#eee',
+               font=('Arial', 15),
+               width=6,
+               height=2,
+               bd=0,
+               command=lambda text=buttons[i][j]: self.button_actions(text),
+            )
+            btn.grid(row=i+1, column=j)
 
-      for i in range(5):
-         for j in range(4):
-            buttons[i][j].grid(row=i+1, column=j)
-      
-
-   def entry_active(self, *args):
+   def clean_when_error(self, *args):
+      '''
+      Function that clean the screen when clicked
+      if the message is an error message.
+      '''
       print('Is it an ERROR message?')
       if self.screen.get() == 'ERROR':
-         print("Yes, deleting...")
          self.screen.delete(0, len(self.screen.get()))
-      else:
-         print("No...")
 
-   def change_text(self, value):
+   def button_actions(self, btn_text):
       if(self.screen.get() == 'ERROR'):
          self.screen.delete(0, len(self.screen.get()))
 
-      if value == 'C':
+      if btn_text == 'C':
          self.screen.delete(0, len(self.screen.get()))
-      elif value == 'CE':
+      elif btn_text == 'CE':
          self.screen.delete(len(self.screen.get())-1, len(self.screen.get()))
-      elif value == '=':
+      elif btn_text == '=':
          self.master.focus()
          if self.screen.get():
             try:
@@ -84,9 +73,9 @@ class MainApp(Frame):
             self.screen.delete(0, len(self.screen.get()))
             self.screen.insert(0, result)
       else:
-         self.screen.insert(len(self.screen.get()), value)
+         self.screen.insert(len(self.screen.get()), btn_text)
 
-root = Tk()
+root = tk.Tk()
 root.config(bg='#111')
 root.resizable(False, False)
 
